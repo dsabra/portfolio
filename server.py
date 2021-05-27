59c66399c6ca679e40db963af56db7e8c3b93d65
Danny Sabra <dsabra@gmail.com>
2021-05-27 12:31:04 -0700

    first commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, url_for, request, redirect
+
+
+app = Flask(__name__)
+print(__name__)
+ 
+@app.route("/")
+def my_home():
+	return render_template('index.html')
+
+#<> make it dynamic so will show any page
+@app.route("/<string:page_name>")
+def html_page(page_name):
+	return render_template(page_name)
+
+
